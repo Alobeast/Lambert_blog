a084ee47d724f04f1a002e68042df85fb4ca412d
Alois Beard <alois.beard@gmail.com>
2019-03-19 19:12:22 +0100

    new and create method for article coded, images can be uploaded

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
 class Article < ApplicationRecord
   belongs_to :user
+
+
+  mount_uploader :main_picture, PhotoUploader
+  mount_uploader :sub_picture1, PhotoUploader
+  mount_uploader :sub_picture_2, PhotoUploader
+  mount_uploader :sub_picture3, PhotoUploader
+  mount_uploader :sub_picture4, PhotoUploader
 end
